1e090702cc7103ab96fb9a792ff458de41721582
Ali Amirhamzeh and Jason Huels <ali@>
2019-08-13 09:29:17 -0700

    demonstrate jquery

---
+++ js/scripts.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+  $("#walrus-hidden .clickable").click(function() {
+    $("#walrus-showing").slideToggle();
+
+
+
+  });
+
+  $("#penguin .clickable").click(function() {
+    $("#devito").slideToggle();
+
+
+
+  });
+
+});
